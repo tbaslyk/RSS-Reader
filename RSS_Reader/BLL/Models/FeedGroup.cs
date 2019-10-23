d58111b1b9e3f7e943e6972a99a85058f71f6452
Tomasz Baslyk <tomekbaslyk@gmail.com>
2019-10-23 19:26:59 +0200

    major changes

---
+++ RSS_Reader/BLL/Models/FeedGroup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Models
+{
+    public class FeedGroup
+    {
+        public List<Feed> Feeds { get; set; }
+
+        public FeedGroup()
+        {
+            Feeds = new List<Feed>();
+        }
+
+        public void Add(Feed feed)
+        {
+            Feeds.Add(feed);
+        } 
+
+    }
+}
---
+++ RSS_Reader/BLL/Services/RSSReader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ServiceModel.Syndication;
+using System.Xml;
+
+namespace BLL
+{
+    public static class RSSReader
+    {
+        public static SyndicationFeed Reader(string url)
+        {
+            SyndicationFeed feed = null;
+
+            using (XmlReader xr = XmlReader.Create(url))
+            {
+                feed = SyndicationFeed.Load(xr);
+            }
+
+            return feed;
+        }
+
+    }
+}
